0281b8a8babf10f4e02205272cceee819fd1aacb
Gurbeer-Sokhi <gsokhi@stevens.edu>
2024-09-19 20:54:50 -0400

    Initial Commit, Project setup

---
+++ src/test/java/sheron/expenser_server_side/ExpenserServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package sheron.expenser_server_side;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenserServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
